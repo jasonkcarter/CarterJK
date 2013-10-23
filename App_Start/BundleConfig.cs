cf21170ff13b29024222f6100482cd93c86bcfe2
Jason Carter <carter.jk@gmail.com>
2013-10-23 10:05:41 -0500

    Setup standard MVC5 WebApi project with HTML5 Shiv, RequireJS, and HTML5 boilerplate CSS

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace CarterJK
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*",
+                        "~/Scripts/require.js"));
+
+            // jQuery
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            // HTML5 shiv bundles
+            bundles.Add(new ScriptBundle("~/bundles/html5shiv").Include(
+                        "~/Scripts/html5shiv.js"));
+            bundles.Add(new ScriptBundle("~/bundles/html5shiv-printshiv").Include(
+                         "~/Scripts/html5shiv-printshiv.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/*.css"));
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarterJK.WebApiApplication" Language="C#" %>
